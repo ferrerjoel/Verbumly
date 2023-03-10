458b6cc00621162cfc4b488155a2743226bd8a08
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2023-03-10 18:26:54 +0100

    Letters that not exists in the word are grayed on the keyboard

--- app/src/main/java/com/example/verbumly/Level.kt
+++ app/src/main/java/com/example/verbumly/Level.kt
@@ -3,36 +3,40 @@ package com.example.verbumly
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.GridView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.verbumly.data.WordData
 import com.example.verbumly.ui_elements.LetterBox
 import com.example.verbumly.ui_elements.LetterBoxAdapter
 
 const val WORD_LINES = 6
+
 class Level : AppCompatActivity() {
 
-    lateinit var gridView: GridView
+    private lateinit var gridView: GridView
 
-    lateinit var letterBoxArray: ArrayList<LetterBox>
-    lateinit var word : String
-    lateinit var adapter : ArrayAdapter<LetterBox>
-    var lettersNum : Int = 0
-    var currentPosition : Int = 0
+    private lateinit var letterBoxArray: ArrayList<LetterBox>
+    lateinit var word: String
+    lateinit var adapter: ArrayAdapter<LetterBox>
+    private var lettersNum: Int = 0
+    private var currentPosition: Int = 0
+
     // When you jump from line this value has to be set to know when you can't return
     // Fist element is the last position available where you can return, max is the max position where you can go
-    private lateinit var lastAndMaxArrayBoxPositions : Pair<Int, Int>
+    private lateinit var lastAndMaxArrayBoxPositions: Pair<Int, Int>
 
-    lateinit var reversedWordArray : CharArray
+    private lateinit var reversedWordArray: CharArray
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_level)
-
+        // Get the saved parameter that is going to be the number of letters the word is going to have
         lettersNum = intent.getIntExtra("letters", 0)
 
-        lastAndMaxArrayBoxPositions = Pair(0, lettersNum-1)
+        lastAndMaxArrayBoxPositions = Pair(0, lettersNum - 1)
 
         WordData.init(context = this)
         word = WordData.getRandomWord(lettersNum)
@@ -43,7 +47,7 @@ class Level : AppCompatActivity() {
         letterBoxArray = ArrayList<LetterBox>()
 
         for (i in 1..lettersNum) {
-            for (j in 1 .. WORD_LINES) {
+            for (j in 1..WORD_LINES) {
                 letterBoxArray.add(LetterBox(this, null))
             }
         }
@@ -62,42 +66,53 @@ class Level : AppCompatActivity() {
      * Gray if the letter doesn't exists, yellow if the letter exists in the word but is not in the correct position
      * and blue if the letter is in the correct position
      */
-    public fun checkWord() {
-        var boxLetter : Char
-        var inputtedWord : String = ""
-        // Get the inputted word on the corresponding line
-        for (i in lettersNum downTo  1){
-            inputtedWord += letterBoxArray[currentPosition - i].letter
-        }
-        // Check if the word can be inputted
-        if (WordData.wordExists(inputtedWord) && currentPosition == lastAndMaxArrayBoxPositions.second+1) {
-            for (i in lettersNum downTo  1) {
-                boxLetter = letterBoxArray[currentPosition - i].letter
-                Log.d("DEBUG", "Letter: $boxLetter")
-                if (boxLetter.lowercaseChar() == reversedWordArray[i-1]) {
-                    letterBoxArray[currentPosition - i].setLetterState(2)
-                    Log.d("DEBUG", "Correct position " + (currentPosition - i))
-                } else if (word.contains(boxLetter, ignoreCase = true)){
-                    Log.d("DEBUG", "Exists $currentPosition")
-                    letterBoxArray[currentPosition - i].setLetterState(1)
-                } else {
-                    letterBoxArray[currentPosition - i].setLetterState(0)
+    fun checkWord() {
+        var boxLetter: Char
+        var inputtedWord: String = ""
+        // If we don't do this check here we may get a out of bounds error (because the word is not completed, for example)
+        if (currentPosition == lastAndMaxArrayBoxPositions.second + 1) {
+            // Get the inputted word on the corresponding line
+            for (i in lettersNum downTo 1) {
+                inputtedWord += letterBoxArray[currentPosition - i].letter
+            }
+            // Check if the word can be inputted
+            if (WordData.wordExists(inputtedWord)) {
+                for (i in lettersNum downTo 1) {
+                    boxLetter = letterBoxArray[currentPosition - i].letter
+                    Log.d("DEBUG", "Letter: $boxLetter")
+                    if (boxLetter.lowercaseChar() == reversedWordArray[i - 1]) {
+                        letterBoxArray[currentPosition - i].setLetterState(2)
+                        Log.d("DEBUG", "Correct position " + (currentPosition - i))
+                    } else if (word.contains(boxLetter, ignoreCase = true)) {
+                        Log.d("DEBUG", "Exists $currentPosition")
+                        letterBoxArray[currentPosition - i].setLetterState(1)
+                    } else {
+                        letterBoxArray[currentPosition - i].setLetterState(0)
+//                        val key = resources.getIdentifier(boxLetter.lowercase() + "_key","id", packageName)
+//                        Log.d("DEBUG", boxLetter + "_key")
+//                        findViewById<Button>(key).setBackgroundColor(ContextCompat.getColor(this, R.color.gray_200))
+                          findViewById<MyKeyboard>(R.id.keyboard).grayOutKey(boxLetter)
+                    }
+                }
+                // Set the new margin of positions where the user can move on the array
+                lastAndMaxArrayBoxPositions = lastAndMaxArrayBoxPositions.copy(
+                    first = lastAndMaxArrayBoxPositions.second + 1,
+                    second = lastAndMaxArrayBoxPositions.second + lettersNum
+                )
+
+                adapter.notifyDataSetChanged()
+
+                if (inputtedWord == word) {
+                    Log.d("DEBUG", "WORD FOUND :D")
                 }
             }
-
-            lastAndMaxArrayBoxPositions = lastAndMaxArrayBoxPositions.copy(first = lastAndMaxArrayBoxPositions.second, second = lastAndMaxArrayBoxPositions.second + lettersNum)
-
-            adapter.notifyDataSetChanged()
-
-            if (inputtedWord == word){
-                Log.d("DEBUG", "WORD FOUND :D")
-            }
         }
+
     }
 
-    public fun addLetter(letter : Char) {
+    fun addLetter(letter: Char) {
 
-        if (lastAndMaxArrayBoxPositions.second >= currentPosition){
+        if (lastAndMaxArrayBoxPositions.second >= currentPosition) {
             letterBoxArray[currentPosition].letter = letter
             // Log.d("DEBUG", letterBoxArray[currentPosition].isCorrect.toString())
             // Log.d("DEBUG", "Letter set " + letter + " " + letterBoxArray[currentPosition].letter)
@@ -108,8 +123,8 @@ class Level : AppCompatActivity() {
 
     }
 
-    public fun deleteLetter() {
-        if (lastAndMaxArrayBoxPositions.first <= currentPosition-1) {
+    fun deleteLetter() {
+        if (lastAndMaxArrayBoxPositions.first <= currentPosition - 1) {
             currentPosition--
             letterBoxArray[currentPosition].letter = ' '
             adapter.notifyDataSetChanged()
