839b636a3f7bb8e031d14bdae24670491a9448b1
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2023-03-24 19:56:32 +0100

    Added credits screen and some fixes

--- app/src/main/java/com/example/verbumly/Credits.kt
+++ app/src/main/java/com/example/verbumly/Credits.kt
@@ -2,6 +2,8 @@ package com.example.verbumly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import com.example.verbumly.fragments.LogoFragment
@@ -12,14 +14,25 @@ class Credits : AppCompatActivity() {
 
     var timer = Timer()
 
+    private lateinit var backButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        setContentView(R.layout.activity_credits)
 
         supportFragmentManager.commit {
             replace<NamesFragment>(R.id.frameContainer)
             setReorderingAllowed(true)
             addToBackStack("replacement")
+        }
+
+        backButton = findViewById(R.id.imageButton)
+
+        
+
+        backButton.setOnClickListener() {
+            timer.cancel()
+            finish()
         }
 
         timer.scheduleAtFixedRate(TimeTask(),3000L,3000L)
