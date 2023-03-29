6c4fd2dd325b7bedcd0a5a99626c2ca786a1dfed
ferrerjoel <95489740+ferrerjoel@users.noreply.github.com>
2023-03-29 19:45:54 +0200

    Compressed leaderboard items and some factorization

--- app/src/main/java/com/example/verbumly/ui_elements/PlayerViewHolder.kt
+++ app/src/main/java/com/example/verbumly/ui_elements/PlayerViewHolder.kt
@@ -16,8 +16,6 @@ import com.example.verbumly.R
 import com.example.verbumly.data.Player
 import com.squareup.picasso.Picasso
 
-const val TEXT_SIZE: Float = 24f
-
 class PlayerViewHolder(view: View, private val leaderboardContext: Context) :
     RecyclerView.ViewHolder(view) {
     // Variables that point to the content of the layout
@@ -27,11 +25,6 @@ class PlayerViewHolder(view: View, private val leaderboardContext: Context) :
 
     fun render(player: Player) {
 
-        playerName.setTextSize(TypedValue.COMPLEX_UNIT_SP, TEXT_SIZE)
-        maxStreak.setTextSize(TypedValue.COMPLEX_UNIT_SP, TEXT_SIZE)
-
-        playerName.typeface = ResourcesCompat.getFont(leaderboardContext, R.font.lemonmilk)
-        maxStreak.typeface = ResourcesCompat.getFont(leaderboardContext, R.font.lemonmilk)
         //Called for every player
         playerName.text = player.name
         // We need the current activity context to get a string, therefore we need to pass the context from Leaderboard -> PlayerAdapter -> PlayerViewHolder
@@ -39,7 +32,7 @@ class PlayerViewHolder(view: View, private val leaderboardContext: Context) :
             leaderboardContext.getString(R.string.leaderboardMaxStreak, player.maxStreak)
         // If the user doesn't have an image we will use the default one
         if (player.photo != null) {
-            Picasso.get().load(player.photo).resize(150, 150).into(playerPhoto)
+            Picasso.get().load(player.photo).resize(100, 100).into(playerPhoto)
         } else {
             playerPhoto.setImageResource(R.drawable.no_profile_user)
         }
